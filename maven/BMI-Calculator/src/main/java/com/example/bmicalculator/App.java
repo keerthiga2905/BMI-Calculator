edfc00fcbae24353d79746a5d9e6284168b8985b
keerthiga2905 <143280604+keerthiga2905@users.noreply.github.com>
2025-08-19 14:02:15 +0530

    Add files via upload

---
+++ maven/BMI-Calculator/src/main/java/com/example/bmicalculator/App.java
@@ -0,0 +1,13 @@
+package com.example.bmicalculator;
+
+
+public class  App {
+
+    
+
+    // Optional main method to test from command line
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
